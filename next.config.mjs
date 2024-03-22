3e228c700ffa9fa5afc42f35f70253f3c138b4e5
Ian Ye <ianye@DN0a1f07a1.SUNet>
2024-03-22 13:21:30 -0700

    final

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "WillowCare",
+    basePath: "/WillowCare",
     output: 'export',
     images: {
         domains: ['lh3.googleusercontent.com'],
