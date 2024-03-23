e21b1cf157f227d18ee9696b3e455eb23b2b851d
Ian Ye <ianye23301@gmail.com>
2024-03-23 13:45:12 -0700

    new

--- app/page.jsx
+++ app/page.jsx
@@ -25,18 +25,18 @@ const Page = () => {
         Welcome to <span style={{ color: 'purple', background: 'linear-gradient(45deg, #7928CA, #FF0080)', WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent' }}>WillowCare</span>
       </h1>
       <p className="mb-8 text-center">Enjoy an all-in-one elder care management software that <br/>tracks your tasks, residents, compliance, and more!</p>
-      <a href="/compliance" className="button text-white py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 w-32 mb-8 text-center">
+      <a href="/service" className="button text-white py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 w-32 mb-8 text-center">
         Get Started
       </a>
       <div className={`flex w-full justify-center mb-12 ${animationLoaded ? 'fade-in' : ''}`}>
-        <a href="/compliance" className="bg-white rounded-lg py-8 px-10 w-1/4 mr-4 text-center">Track your compliance</a>
+        <a href="/" className="bg-white rounded-lg py-8 px-10 w-1/4 mr-4 text-center">Track your compliance</a>
         <div className="w-1/6"></div>
-        <a href="/residents" className="bg-white rounded-lg py-8 px-10 w-1/4 text-center">Manage residents and move-ins</a>
+        <a href="/" className="bg-white rounded-lg py-8 px-10 w-1/4 text-center">Manage residents and move-ins</a>
       </div>
       <div className={`flex w-full justify-center ${animationLoaded ? 'fade-in' : ''}`}>
-        <a href="/tasks" className="bg-white rounded-lg py-8 px-10 w-1/4 mr-4 text-center">Manage and review personal tasks</a>
+        <a href="/" className="bg-white rounded-lg py-8 px-10 w-1/4 mr-4 text-center">Manage and review personal tasks</a>
         <div className="w-1/6"></div>
-        <a href="/analytics" className="bg-white rounded-lg py-8 px-10 w-1/4 text-center">Analytics</a>
+        <a href="/" className="bg-white rounded-lg py-8 px-10 w-1/4 text-center">Analytics</a>
       </div>
     </div>
   );
