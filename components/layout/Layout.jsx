edf70a48badf142bc47b6a59f9f9d9b1d0992531
Steven_Zhou <zhouster8558@gmail.com>
2024-05-10 12:41:20 -0700

    initial changes

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -22,18 +22,18 @@ const Layout = ({ children }) => {
     const { data: session, status: sessionStatus } = useSession();
     const loading = sessionStatus === 'loading';
 
-    return ( portal_side ? (
+    return (portal_side ? (
         <div className='flex flex-col'>
             <div className="border-b flex flex-row">
-                <img 
-                src='/assets/images/logo.png'
-                alt="logo"
-                width={120}
-                height={120}
-                className='mx-7 my-4'
+                <img
+                    src='/assets/images/logo.png'
+                    alt="logo"
+                    width={120}
+                    height={120}
+                    className='mx-7 my-4'
                 />
             </div>
-           
+
             <div>
                 {children}
             </div>
@@ -47,7 +47,7 @@ const Layout = ({ children }) => {
 
             <div className="flex-1 flex flex-col">
                 <div className='flex items-center search sticky top-0 pr-4'>
-                    <Searchbar onEnterKeyPress={onEnterKeyPress}/>
+                    <Searchbar onEnterKeyPress={onEnterKeyPress} />
                     {/* <Profile/> */}
 
                     {!loading && (session?.user ? (
@@ -55,16 +55,16 @@ const Layout = ({ children }) => {
                             <button type='button' onClick={signOut} className='outline_btn login w-32 h-7'>
                                 Sign Out
                             </button>
-                            
-                                <Image
-                                    src={session?.user.image}
-                                    width={50}
-                                    height={50}
-                                    className='rounded-full p-1'
-                                    alt='profile'
-                                    layout='fixed' 
-                                    priority
-                                />
+
+                            <Image
+                                src={session?.user.image}
+                                width={50}
+                                height={50}
+                                className='rounded-full p-1'
+                                alt='profile'
+                                layout='fixed'
+                                priority
+                            />
                         </div>
                     ) : (
                         <div className='flex gap-3'>
@@ -75,7 +75,7 @@ const Layout = ({ children }) => {
                     ))}
                 </div>
                 <div className='flex-1 flex flex-col h-full background'>
-                    {children} 
+                    {children}
                 </div>
             </div>
         </div>
